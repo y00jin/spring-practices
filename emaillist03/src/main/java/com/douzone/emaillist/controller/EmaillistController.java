58977ef2a246ae82f26100eac43fe9dc07b4a9f1
B <wjs2510@icloud.com>
2020-02-21 10:31:19 +0900

    init emaillist03

---
+++ emaillist03/src/main/java/com/douzone/emaillist/controller/EmaillistController.java
@@ -0,0 +1,16 @@
+package com.douzone.emaillist.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class EmaillistController {
+	
+	@ResponseBody
+	@RequestMapping("")
+	public String index() {
+		return "hello";
+	}
+	
+}
