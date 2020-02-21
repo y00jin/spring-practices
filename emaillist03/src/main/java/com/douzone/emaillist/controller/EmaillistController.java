1533a75de658744c7b57fc354ac118a96c266118
B <wjs2510@icloud.com>
2020-02-21 11:39:11 +0900

    update emaillist03

--- emaillist03/src/main/java/com/douzone/emaillist/controller/EmaillistController.java
+++ emaillist03/src/main/java/com/douzone/emaillist/controller/EmaillistController.java
@@ -1,16 +1,41 @@
 package com.douzone.emaillist.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.douzone.emaillist.repository.EmaillistRepository;
+import com.douzone.emaillist.vo.EmaillistVo;
 
 @Controller
 public class EmaillistController {
 	
-	@ResponseBody
+	@Autowired
+	private EmaillistRepository emaillistRepository; 
+	
+//	@ResponseBody
 	@RequestMapping("")
-	public String index() {
-		return "hello";
+	public String index(Model model) {
+		
+		List<EmaillistVo> list = emaillistRepository.findAll();
+		model.addAttribute("list", list);
+
+		return "/WEB-INF/views/index.jsp";
+	}
+	
+	@RequestMapping(value="/add", method=RequestMethod.GET)
+	public String add() {
+		return "/WEB-INF/views/add.jsp";
+	}
+	
+	@RequestMapping(value="/add", method=RequestMethod.POST)
+	public String add(EmaillistVo emaillistVo) {
+		emaillistRepository.insert(emaillistVo);
+		return "redirect:/";
 	}
 	
 }
