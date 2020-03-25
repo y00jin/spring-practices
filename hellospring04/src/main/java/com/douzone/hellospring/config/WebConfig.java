48cd3961fa103f69eede0d7097a8ffff5c762df2
B <wjs2510@icloud.com>
2020-03-25 15:48:34 +0900

    logex modify pon.xml

---
+++ hellospring04/src/main/java/com/douzone/hellospring/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.douzone.hellospring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan({"com.douzone.hellospring.controller"})
+public class WebConfig {
+	
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		resolver.setExposeContextBeansAsAttributes(true);
+		return resolver;
+	}
+	
+}
