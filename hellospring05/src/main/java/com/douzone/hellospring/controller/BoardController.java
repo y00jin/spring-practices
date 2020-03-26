f4ebe10d90b1a0895ea6f0bbb0c50f09e620be98
B <wjs2510@icloud.com>
2020-03-26 09:25:20 +0900

    delete web.xml using WebApplicationInitializer

---
+++ hellospring05/src/main/java/com/douzone/hellospring/controller/BoardController.java
@@ -0,0 +1,27 @@
+package com.douzone.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/*
+ * @RequesetMapping
+ * 메소드 단독매핑
+ */
+@Controller
+public class BoardController {
+	
+	@ResponseBody
+	@RequestMapping("/board/write")
+	public String write() {
+		return "BoardController:write";
+	}
+
+	@ResponseBody
+	@RequestMapping("/board/view/{no}")
+	public String view(@PathVariable("no") Long no) {
+		System.out.println("no : " + no);
+		return "BoardController:view";
+	}
+}
